fb454cb590c2f1d1dff37c3551c8124178f2232d
tstokvis <tmstokvis@gmail.com>
2017-05-15 08:06:40 -0700

    added exception handling

--- Raspberry/app/src/main/java/tstok/raspberry/RaspberryActivity.kt
+++ Raspberry/app/src/main/java/tstok/raspberry/RaspberryActivity.kt
@@ -1,8 +1,10 @@
 package tstok.raspberry
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.GpioCallback
@@ -11,48 +13,67 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.io.IOException
 
 
 class RaspberryActivity : AppCompatActivity() {
 
     val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-    lateinit var greenGpio: Gpio
-    lateinit var greenButton: Gpio
+    lateinit var ledGpio: Gpio
+    lateinit var buttonGpio: Gpio
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.demo_activity)
         displayIOPorts()
 
         val manager = PeripheralManagerService()
-        greenGpio = manager.openGpio(GREEN_LED_PIN)
-        greenButton = manager.openGpio(GREEN_BUTTON_PIN)
+        ledGpio = manager.openGpio(LED_PIN)
+        buttonGpio = manager.openGpio(BUTTON_PIN)
 
-        // direction out for lights (digital write)
-        greenGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+        var ledSuccessful = true
+        var buttonSuccessful = true
 
         // direction in for buttons (digital read)
-        greenGpio.setDirection(Gpio.DIRECTION_IN)
-        greenGpio.setActiveType(Gpio.ACTIVE_LOW)
-        greenGpio.setEdgeTriggerType(Gpio.EDGE_RISING)
-        greenGpio.registerGpioCallback(DemoCallback())
-
-        // listener for when UI element is clicked, changes LED state on click
-        findViewById(R.id.green_button).setOnClickListener {
-            greenGpio.value = !greenGpio.value
-            updateFirebase()
+        try {
+            buttonGpio.setDirection(Gpio.DIRECTION_IN)
+            buttonGpio.setActiveType(Gpio.ACTIVE_LOW)
+            buttonGpio.setEdgeTriggerType(Gpio.EDGE_RISING)
+            buttonGpio.registerGpioCallback(DemoCallback())
+        } catch (exception: IOException) {
+            buttonSuccessful = false
         }
 
-        database.getReference(GREEN).addValueEventListener(LightChangeListener())
+        // direction out for lights (digital write)
+        try {
+            ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+
+            // listener for when UI element is clicked, changes LED state on click
+            findViewById(R.id.green_button).setOnClickListener {
+                ledGpio.value = !ledGpio.value
+                findViewById(R.id.light_circle).visibility = if (ledGpio.value) View.VISIBLE else View.INVISIBLE
+                updateFirebase()
+            }
+
+            database.getReference(GREEN).addValueEventListener(LightChangeListener())
+        } catch (exception: IOException) {
+            ledSuccessful = false
+        }
+
+        AlertDialog.Builder(this)
+                .setMessage("LED Sucessful: $ledSuccessful, Button Successful: $buttonSuccessful")
+                .setPositiveButton(if (!ledSuccessful && !buttonSuccessful) "Fuck!" else "Okay", null)
+                .show()
 
         super.onCreate(savedInstanceState)
     }
 
     override fun onDestroy() {
+        ledGpio.close()
+        buttonGpio.close()
         super.onDestroy()
-        greenGpio.close()
     }
 
-    fun updateFirebase() = database.getReference(GREEN).setValue(greenGpio.value)
+    fun updateFirebase() = database.getReference(GREEN).setValue(ledGpio.value)
 
     /**
      *  Displays available IO ports at the top of the screen
@@ -73,7 +94,8 @@ class RaspberryActivity : AppCompatActivity() {
      */
     open inner class DemoCallback : GpioCallback() {
         override fun onGpioEdge(gpio: Gpio?): Boolean {
-            greenGpio.value = !greenGpio.value
+            ledGpio.value = !ledGpio.value
+            findViewById(R.id.light_circle).visibility = if (ledGpio.value) View.VISIBLE else View.INVISIBLE
             updateFirebase()
             return true
         }
@@ -88,14 +110,15 @@ class RaspberryActivity : AppCompatActivity() {
         }
 
         override fun onDataChange(dataSnapshot: DataSnapshot?) {
-            greenGpio.value = dataSnapshot?.getValue(Boolean::class.java).isTrue()
+            ledGpio.value = dataSnapshot?.getValue(Boolean::class.java).isTrue()
+            findViewById(R.id.light_circle).visibility = if (ledGpio.value) View.VISIBLE else View.INVISIBLE
         }
     }
 
     companion object {
 
-        const val GREEN_LED_PIN = "BCM12"
-        const val GREEN_BUTTON_PIN = "BCM20"
+        const val LED_PIN = "BCM20"
+        const val BUTTON_PIN = "BCM21"
         const val GREEN = "green"
     }
 }
